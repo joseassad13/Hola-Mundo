aeed2c3b1d342691a8e3f9320520927dae6b1207
joseassad13 <159552383+joseassad13@users.noreply.github.com>
2024-02-23 02:07:29 +0000

    Version 2

---
+++ include/Alimento.hpp
@@ -0,0 +1,13 @@
+class Alimento
+{
+private:
+    int energia;   
+public:
+    Alimento() {
+        this -> energia = energia;
+    }
+    ~Alimento() {}
+    int ExtraerEnergia(){
+        return energia;
+    }
+};
